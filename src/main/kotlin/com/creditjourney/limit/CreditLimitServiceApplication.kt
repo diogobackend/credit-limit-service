9c0e8b92074e74a65c6fd71e65aa42608354f691
idd_dsferreira <idd_dsferreira@uolinc.com>
2026-07-18 17:41:25 -0300

    started project

---
+++ src/main/kotlin/com/creditjourney/limit/CreditLimitServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.creditjourney.limit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditLimitServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditLimitServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/creditjourney/limit/CreditLimitServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.creditjourney.limit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditLimitServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
